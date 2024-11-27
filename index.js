6588407dbce12bbcdefd146f36614a13d78d5109
kenaryark <kenarya5@gmail.com>
2024-11-27 18:40:53 +0700

    enable cors & add access control

--- index.js
+++ index.js
@@ -25,12 +25,27 @@ mongoose.connection.on("disconnected", () => {
 const corsOptions = {
   origin: ["http://localhost:5173"], // Add your frontend URL
   credentials: true, // Allow cookies to be sent
+  //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
 };
 
 //middlewares
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
+
+// Middleware Global untuk Header CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
